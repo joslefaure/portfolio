812069a167074d7252eda2ff02bed268421f184f
Gueter Josmy Faure <josmyfaure@gmail.com>
2019-12-29 18:42:12 +0800

    Updates

---
+++ precache-manifest.3958eb57a85fd43b6d42643c456610ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "d46e4382dd754770e4de",
+    "url": "/portfolio/static/js/main.ae73f0db.chunk.js"
+  },
+  {
+    "revision": "cdbabc34896f1f48ea90",
+    "url": "/portfolio/static/js/2.ecd37079.chunk.js"
+  },
+  {
+    "revision": "d46e4382dd754770e4de",
+    "url": "/portfolio/static/css/main.7bab048f.chunk.css"
+  },
+  {
+    "revision": "e89811355e9ac8e217d50bde83647fa7",
+    "url": "/portfolio/index.html"
+  }
+];
