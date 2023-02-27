880269a92d44d609f236f5d19c33d7c9c61bb04e
103071073 <josmyfaure@gmail.com>
2023-02-27 17:12:42 +0800

    Updates

--- precache-manifest.1da4cff06440cf2321fb4ad4339c1e2e.js
+++ precache-manifest.1da4cff06440cf2321fb4ad4339c1e2e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
   },
   {
-    "revision": "e2484b5d5c3da1d80c05",
-    "url": "/portfolio/static/js/main.07d268cd.chunk.js"
+    "revision": "ef15e890ff14eb08de36",
+    "url": "/portfolio/static/js/main.5187cee8.chunk.js"
   },
   {
     "revision": "cdbabc34896f1f48ea90",
     "url": "/portfolio/static/js/2.ecd37079.chunk.js"
   },
   {
-    "revision": "e2484b5d5c3da1d80c05",
+    "revision": "ef15e890ff14eb08de36",
     "url": "/portfolio/static/css/main.7bab048f.chunk.css"
   },
   {
-    "revision": "019685654b88b3f5ddd07ec474577f79",
+    "revision": "7911458305d12a1732406af470ae4ea1",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d9d6b8b99246b9eebfc852f227ff32a3.js"
+  "/portfolio/precache-manifest.1da4cff06440cf2321fb4ad4339c1e2e.js"
 );
 
 workbox.clientsClaim();
